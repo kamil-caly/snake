ef657eaf23ddba60ef0b247b55a70f7621427b65
Kamil Cały <kamil.caly@vp.pl>
2023-09-15 22:47:07 +0200

    drawing game over menu / fixed checking snake collision / finished game

--- snake.py
+++ snake.py
@@ -32,6 +32,19 @@ class UI():
         score_rect = surface.get_rect(center=(self.width / 2, 30))
         self.screen.blit(surface, score_rect)
 
+    def draw_final_text(self, font, text_color, points):
+        game_over = font.render(f'Game Over', True, text_color)
+        rect = game_over.get_rect(center=(self.width / 2, 100))
+        self.screen.blit(game_over, rect)
+
+        finale_score = font.render(f'Final Score: {points}', True, text_color)
+        rect = finale_score.get_rect(center=(self.width / 2, 200))
+        self.screen.blit(finale_score, rect)
+
+        again_text = font.render(f'Press space to play again...', True, text_color)
+        rect = again_text.get_rect(center=(self.width / 2, 300))
+        self.screen.blit(again_text, rect)
+
     def draw_snake(self, snake_parts):
         big_size = 50
         small_size = 44
@@ -44,7 +57,9 @@ class UI():
             self.screen.blit(big_rect, (p[0] * big_size, p[1] * big_size + 50))
             self.screen.blit(small_rect, (p[0] * big_size + int((big_size - small_size) / 2), p[1] * big_size + int((big_size - small_size) / 2) + 50))
 
-
+    def draw_game_over(self, points):
+        self.screen.blit(self.background, (0, 0))
+        self.draw_final_text(pygame.font.Font('font/Pixeltype.ttf', 50), (200, 200, 0), points)
 
 class Snake():
     def __init__(self, x, y, game_width, game_height):
@@ -94,13 +109,13 @@ class Snake():
 
     def is_collision(self):
         head = self.parts[0]
-        for i in range(1, self.parts):
+        for i in range(1, len(self.parts)):
             if self.parts[i] == head:
                 return True
 
-        if head[0] + 1 >= self.game_width or head[0] <= 0:
+        if head[0] >= self.game_width or head[0] < 0:
             return True
-        if head[1] + 1 >= self.game_height or head[1] <= 0:
+        if head[1] + 1 >= self.game_height or head[1] < 0:
             return True
 
         return False
@@ -141,6 +156,7 @@ class Game_Logic():
         self.ui = UI(self.game_width, self.game_height)
         self.apple = Apple(self.snake.parts, self.game_width / 50, self.game_height / 50 - 1)
         self.is_running = True
+        self.is_game_over = False
         self.points = 0
         self.current_game_speed, self.game_speed = 1, 1
 
@@ -160,18 +176,32 @@ class Game_Logic():
                         self.snake.change_dir('left')
                     elif event.key == pygame.K_w:
                         self.snake.change_dir('up')
+                    elif event.key == pygame.K_SPACE:
+                        if self.is_game_over:
+                            self.is_game_over = False
+                            self.snake = Snake(5, 0, self.game_width / 50, self.game_height / 50)
+                            self.ui = UI(self.game_width, self.game_height)
+                            self.apple = Apple(self.snake.parts, self.game_width / 50, self.game_height / 50 - 1)
+                            self.points = 0
 
-            if self.current_game_speed <= 0:
-                self.current_game_speed = self.game_speed
-                self.snake.move()
-            self.current_game_speed -= 0.1
+            if self.is_game_over:
+                self.ui.draw_game_over(self.points)
 
-            if self.snake.is_hit_point(self.apple.x, self.apple.y):
-                self.snake.grow()
-                self.apple = Apple(self.snake.parts, self.game_width / 50, self.game_height / 50 - 1)
-                self.points += 1
+            else:
+                if self.current_game_speed <= 0:
+                    self.current_game_speed = self.game_speed
+                    self.snake.move()
+                self.current_game_speed -= 0.1
 
-            self.ui.draw(self.points, self.snake.parts, [self.apple.x, self.apple.y])
+                if self.snake.is_hit_point(self.apple.x, self.apple.y):
+                    self.snake.grow()
+                    self.apple = Apple(self.snake.parts, self.game_width / 50, self.game_height / 50 - 1)
+                    self.points += 1
+
+                if self.snake.is_collision():
+                    self.is_game_over = True
+
+                self.ui.draw(self.points, self.snake.parts, [self.apple.x, self.apple.y])
 
             pygame.display.update()
             self.clock.tick(60)
